fe8d354bafcb84da1dc6de8e7b2ab818ff75351c
Sarah Gustafson <sgustafson80@gmail.com>
2019-09-03 20:25:00 +0000

    will paginate 10 places per page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,7 +4,7 @@ class PlacesController < ApplicationController
 
 
   def index
-    @places = Place.paginate(page: params[:page], per_page: 1)
+    @places = Place.paginate(page: params[:page], per_page: 10)
   end
 
   def new
